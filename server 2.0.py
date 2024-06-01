5621d59801220e94eef5a7008572a0ad901302b8
lucas123lucash <139007233+lucas123lucash@users.noreply.github.com>
2024-06-01 03:29:20 +0000

    Mundo guardado 2024-06-01

--- server 2.0.py
+++ server 2.0.py
@@ -1,19 +1,22 @@
-import requests,os
-def download_latest_release(repo_owner,repo_name,download_path='.'):
-	F=f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases/latest";B=requests.get(F);A=''
-	if B.status_code==200:
-		G=B.json();C=G.get('assets')
-		if C:
-			D=C[0];E=D.get('browser_download_url')
-			if E:
-				A=os.path.join(download_path,D.get('name'))
-				with open(A,'wb')as H:H.write(requests.get(E).content)
-				print(f"Download complete! File saved as: {A}")
-			else:print('No files found to download in the last release.')
-		else:print('No assets were found in the last release.')
-	else:print('Error getting information about the latest release.')
-	return A
-repo_owner='elyxdev'
-repo_name='elyx-server-eng'
-flnm=download_latest_release(repo_owner,repo_name)
-os.system(f"python3 {flnm}")
+import requests,os,base64
+if not os.path.exists("./.gitignore"):
+	big = "L3dvcmtfYXJlYQ0KL3NlcnZpZG9yX21pbmVjcmFmdA0KL21pbmVjcmFmdF9zZXJ2ZXINCi9zZXJ2aWRvcl9taW5lY3JhZnRfb2xkDQovdGFpbHNjYWxlLWNzDQovdGhhbm9zDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioub3V0cHV0"
+	dec = base64.standard_b64decode(big).decode()
+	with open(".gitignore", 'w') as giti:
+		giti.write(dec)
+def download_latest_release(download_path='.'):
+	mirror = "https://elyxdev.github.io/latest"
+	pet = requests.get(mirror)
+	if pet.status_code == 200:
+		data = pet.json()
+		url = data.get('url')
+		version = url.split("/")[-1]
+		pathto = os.path.join(download_path, version)
+		with open(pathto, 'wb') as archivo:
+			archivo.write(requests.get(url).content)
+		return version
+flnm=download_latest_release()
+if flnm.split(".")[-1] == "pyc":
+    os.system(f"python3 {flnm}")
+else:
+	os.system(f"chmod +x {flnm} && ./{flnm}")
